a3c4b3a3ec5b758d5024b0af67afd3a560f18fbf
hashmmaheaalam <haalam@dminc.com>
2022-03-25 21:06:35 +0530

    This program is for condition flow control which is if

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
 
-public class test {
+public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
+		int a=10;
+		if(a==21)
+			System.out.println("condition matched");
+		else
+			System.out.println("condition not matched");
 	}
 }
 
