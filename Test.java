55a631e83e27e6c512fb9a08940c32bf5343d4f1
hashmmaheaalam <haalam@dminc.com>
2022-03-28 13:09:34 +0530

    for loop syntax

--- Test.java
+++ Test.java
@@ -1,14 +1,8 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
-		int a=10;
-		switch(a){
-			case 10: System.out.println("Case ten");
-			 break;
-			case 12: System.out.println("Case twelve");
-			 break;
-			default : System.out.println("Default case");
-			 break;
+		for(int i=10; i<=9;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+			System.out.println("This is for loop");
 		}
 	}
 }
