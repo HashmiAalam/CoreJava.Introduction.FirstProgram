1919dd6399071ce5d27bc086171883a081af88cd
hashmmaheaalam <haalam@dminc.com>
2022-03-28 19:55:50 +0530

    for loop in infinate state when there is no break and condition

--- Test.java
+++ Test.java
@@ -2,10 +2,11 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
 		
-		for(int i=10,b=11; i<=12;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			if(b==12)                          //we can not initialization two diffrent data type like int i=10, byte b=12 it will give error
+		for(int i=10,b=11; ;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+			//if(b==12)            //if we don't give any condition in for loop than JVM will consider condition as true and for loop goes into infinte loop
+           // break;				
 		System.out.println("This is for loop with initialization");
-	b++;
+	//b++;
 		}
 	}
 }
