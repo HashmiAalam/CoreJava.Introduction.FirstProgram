c12f7d6c8695a8c7591ad44a0d6b581ef918f36e
hashmmaheaalam <haalam@dminc.com>
2022-03-28 18:13:46 +0530

    In for loop intialization part is optional

--- Test.java
+++ Test.java
@@ -1,8 +1,9 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
-		for(int i=10; i<=9;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			System.out.println("This is for loop");
+		int i=10;
+		for(; i<=12;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+			System.out.println("This is for loop without initialization");
 		}
 	}
 }
