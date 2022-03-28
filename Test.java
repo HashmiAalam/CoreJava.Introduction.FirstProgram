760d73837e70ba7145fe2daa3f5a310be809673a
hashmmaheaalam <haalam@dminc.com>
2022-03-28 21:01:03 +0530

    infinte for loop:- when there is not condition, no intializationand no increment/decrement

--- Test.java
+++ Test.java
@@ -2,9 +2,9 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
 		
-		for(int i=10;i<=12 ;System.out.println("Hi increment/decrement in for loop")){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			System.out.println("This is for loop with no break i.e no increment/decrement for condition to become false");
-	i++;   //program will not fall into infinite condition i++ will work as break to loop
+		for(; ;){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+			System.out.println("This is for loop with no break, no condition no initialization i.e no increment/decrement for condition to become false");
+	  //program will fall into infinite condition 
 		}
 	}
 }
