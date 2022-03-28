fa6c1cc5b45d6fe8c29a4858368a196e2b429482
hashmmaheaalam <haalam@dminc.com>
2022-03-28 20:49:40 +0530

    In for loop we can give increment/decrement after some flow

--- Test.java
+++ Test.java
@@ -3,10 +3,8 @@ public class Test {
 		System.out.println("Aalam world");
 		
 		for(int i=10;i<=12 ;){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			//program will fall in infinite condition
-		
-		System.out.println("This is for loop with no break i.e no increment/decrement for condition to become false");
-	break;
+			System.out.println("This is for loop with no break i.e no increment/decrement for condition to become false");
+	i++;   //program will not fall into infinite condition i++ will work as break to loop
 		}
 	}
 }
