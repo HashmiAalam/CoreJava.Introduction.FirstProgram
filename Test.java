671d0155073638d160ab77a575702e1a87c588c0
hashmmaheaalam <haalam@dminc.com>
2022-03-28 19:58:59 +0530

    We can but condition using if statement

--- Test.java
+++ Test.java
@@ -3,10 +3,10 @@ public class Test {
 		System.out.println("Aalam world");
 		
 		for(int i=10,b=11; ;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			//if(b==12)            //if we don't give any condition in for loop than JVM will consider condition as true and for loop goes into infinte loop
-           // break;				
+			if(b==12)            //if we don't give any condition in for loop than JVM will consider condition as true and for loop goes into infinte loop
+           break;				
 		System.out.println("This is for loop with initialization");
-	//b++;
+	b++;
 		}
 	}
 }
