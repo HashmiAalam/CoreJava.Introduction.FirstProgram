4c15dea2a3b63ba7d912fa42af830fe10182fa77
hashmmaheaalam <haalam@dminc.com>
2022-03-28 18:17:36 +0530

    We can write n no of sysout statments in intialization part of for loop with comma sepration

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
 		int i=10;
-		for(; i<=12;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+		for(System.out.println("This is initialization"); i<=12;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
 			System.out.println("This is for loop without initialization");
 		}
 	}
