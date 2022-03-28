a09c0a725fc35826fb2a5608da83b8b25e5cb9f2
hashmmaheaalam <haalam@dminc.com>
2022-03-28 20:52:00 +0530

    We can give sysout in increment/decrement position in for loop

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
 		
-		for(int i=10;i<=12 ;){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+		for(int i=10;i<=12 ;System.out.println("Hi increment/decrement in for loop")){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
 			System.out.println("This is for loop with no break i.e no increment/decrement for condition to become false");
 	i++;   //program will not fall into infinite condition i++ will work as break to loop
 		}
