22acb86bf23db088ee4fcf9f14a684e25ca5ecbd
hashmmaheaalam <haalam@dminc.com>
2022-03-28 19:39:16 +0530

    We can intialize n no of same datatypes but we can not initialize different data type in for loop

--- Test.java
+++ Test.java
@@ -1,9 +1,11 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
-		int i=10;
-		for(System.out.println("This is initialization"); i<=12;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			System.out.println("This is for loop without initialization");
+		
+		for(int i=10,b=11; i<=12;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+			if(b==12)                          //we can not initialization two diffrent data type like int i=10, byte b=12 it will give error
+		System.out.println("This is for loop with initialization");
+	b++;
 		}
 	}
 }
