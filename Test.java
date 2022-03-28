dd39b82522c77c7075feefe1eac9ce973f5f17e9
hashmmaheaalam <haalam@dminc.com>
2022-03-28 20:41:42 +0530

    for loop has no increment or decrement as it is optional but program fall into infinite loop

--- Test.java
+++ Test.java
@@ -2,11 +2,11 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
 		
-		for(int i=10,b=11; ;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			if(b==12)            //if we don't give any condition in for loop than JVM will consider condition as true and for loop goes into infinte loop
-           break;				
-		System.out.println("This is for loop with initialization");
-	b++;
+		for(int i=10;i<=12 ;){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+			//program will fall in infinite condition
+		
+		System.out.println("This is for loop with no break i.e no increment/decrement for condition to become false");
+	
 		}
 	}
 }
