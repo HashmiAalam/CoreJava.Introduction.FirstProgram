8429504687ae0511872131c33919f95e74c7ace3
hashmmaheaalam <haalam@dminc.com>
2022-03-28 21:14:38 +0530

    In for loop if condition given as false then we get compilation error as rest of the program is in unreachable state

--- Test.java
+++ Test.java
@@ -2,10 +2,9 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
 		
-		for(int i=10;true;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			System.out.println("This is for loop has no break,  condition will not become false");
+		for(int i=10;false;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
+			System.out.println("This for loop has false condition program will become unreachable statement");
 	  //program will give compilation error 
 		}
-		System.out.println("Rest of the program will become unreachable statement");
 	}
 }
