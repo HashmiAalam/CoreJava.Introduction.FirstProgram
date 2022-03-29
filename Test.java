88d987820fb73753c09f0c4aabd4c2241c91aed2
hashmmaheaalam <haalam@dminc.com>
2022-03-29 15:26:09 +0530

    This is a plane example of continue statement to skip current loop

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ public class Test {
 	public static void main(String[] args){
 		for(int i=10;i<20;i++){
 			if(i==15)
-				break;
-		System.out.println(i);   }
+				continue;   //in output we will not get 15
+		System.out.println(i);   //Whenever continue statement is executed it skip rest of the code of current loop
+		}
 	}
 }
