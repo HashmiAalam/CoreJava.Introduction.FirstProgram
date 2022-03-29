95c52ad087b27688fe1e0e658d3a5df931e65f0c
hashmmaheaalam <haalam@dminc.com>
2022-03-29 13:11:42 +0530

    in while loop if condition given true manually and there is no logic to alter the condition then rest of the program will become unreachable statement

--- Test.java
+++ Test.java
@@ -2,9 +2,11 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
 		int i=1;
-		while(i<5){
-		System.out.println("Hi there");  //since there is no break in the progaram loop will fall into infinte condition
+		while(true){            //if condition is give false manually then while loop will become unreachable statement
+		System.out.println("Hi there");  //since the condition is no true in the progaram loop will fall into infinte condition and we have rest of code so we get error
 		i++;
 		}
+		System.out.println("Rest of application");
 	}
 }
+//error:-on line 9 unreachable statement
