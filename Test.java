51237936f21c5625bd45008ddd8cf992b7a9217a
hashmmaheaalam <haalam@dminc.com>
2022-03-29 13:30:29 +0530

    do while loop is for executing first and then check condition'

--- Test.java
+++ Test.java
@@ -1,12 +1,9 @@
 public class Test {
 	public static void main(String[] args){
-		System.out.println("Aalam world");
 		int i=1;
-		while(true){            //if condition is give false manually then while loop will become unreachable statement
-		System.out.println("Hi there");  //since the condition is no true in the progaram loop will fall into infinte condition and we have rest of code so we get error
+		do{
+		System.out.println("This is do while");
 		i++;
-		}
-		System.out.println("Rest of application");
+		}while(i<5);
 	}
 }
-//error:-on line 9 unreachable statement
