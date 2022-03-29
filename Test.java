12cb5478a3527b69c414f05cb45065b27186f208
hashmmaheaalam <haalam@dminc.com>
2022-03-29 13:00:11 +0530

    While loop without break:- If any loop statement do not get breaking point it will fall into infinte state

--- Test.java
+++ Test.java
@@ -1,10 +1,9 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("Aalam world");
-		
-		for(int i=10;false;i++){          //for( initialization optional; condition optional(default value is true) ; increment/decrement optional) 
-			System.out.println("This for loop has false condition program will become unreachable statement");
-	  //program will give compilation error 
+		int i=1;
+		while(i<5){
+		System.out.println("Hi there");  //since there is no break in the progaram loop will fall into infinte condition
 		}
 	}
 }
