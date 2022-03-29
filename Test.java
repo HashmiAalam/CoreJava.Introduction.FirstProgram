a14860f504e7c473565f4b82fa00b601958b3974
hashmmaheaalam <haalam@dminc.com>
2022-03-29 15:15:37 +0530

    This is a plane example of break statement to break loop

--- Test.java
+++ Test.java
@@ -1,10 +1,8 @@
 public class Test {
 	public static void main(String[] args){
-		int i=1;
-		do{
-		System.out.println("This is do while");
-		i++;
-		}while(false);
-		System.out.println("Rest of the program");   //there is no error in this program
+		for(int i=10;i<20;i++){
+			if(i==15)
+				break;
+		System.out.println(i);   }
 	}
 }
